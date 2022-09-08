22ffeeb7ef0b29b20482840424d868068d336910
Shawn Moss <Shawn.Moss@pcg.com>
2022-09-08 11:26:55 -0500

    updates

---
+++ .storybook/preview.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { GlobalStyle } from '../src/shared/global';
+
+/*
+ * Global decorator to apply the styles to all stories
+ * Read more about them at:
+ * https://storybook.js.org/docs/react/writing-stories/decorators#global-decorators
+ */
+export const decorators = [
+  Story => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  ),
+];
+
+/*
+ * Read more about global paramters at:
+ * https://storybook.js.org/docs/react/writing-stories/parameters#global-parameters
+ */
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
